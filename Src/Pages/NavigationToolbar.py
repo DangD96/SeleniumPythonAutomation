4cc30d915d5bd5a61df29c161eae1b2743966949
DangD96 <35606061+DangD96@user.noreply.github.com>
2023-06-12 20:52:00 -0500

    add waits

--- Src/Pages/NavigationToolbar.py
+++ Src/Pages/NavigationToolbar.py
@@ -15,15 +15,18 @@ class NavigationToolbar(object):
 
     # Page methods/services
     def hoverCompanyTab(self):
-            WebDriverWait(self.driver, 10).until(lambda x: x.find_element(*self.COMPANY_TAB), message="Could not find element.") # Wait until the function returns something that evals to True
-            actions = ActionChains(self.driver)
-            actions.move_to_element(self.driver.find_element(*self.COMPANY_TAB)) # unpack the COMPANY_TAB tuple into find_element's two parameters
-            actions.perform()
-            return NavigationToolbar(self.driver)
+        # Wait until the callable function returns something that evals to True
+        # Use * to unpack the COMPANY_TAB tuple into find_element's two parameters
+        WebDriverWait(self.driver, 10).until(lambda x: x.find_element(*self.COMPANY_TAB), message="Could not find element.") 
+        companyTab = self.driver.find_element(*self.COMPANY_TAB)
+        actions = ActionChains(self.driver)
+        actions.move_to_element(companyTab) 
+        actions.perform()
+        return NavigationToolbar(self.driver)
 
     def clickContactUs(self):
-        self.driver.find_element(*self.COMPANY_TAB_CONTACT_US).click()
-        return ContactUsPage(self.driver) # Return ContactUsPage object because this action takes us to that "page" now
-    
-    
+        WebDriverWait(self.driver, 10).until(lambda x: x.find_element(*self.COMPANY_TAB_CONTACT_US), message="Could not find element.")
+        contactUsLink = self.driver.find_element(*self.COMPANY_TAB_CONTACT_US)
+        self.driver.find_element(contactUsLink).click()
+        return ContactUsPage(self.driver)                # Return ContactUsPage object because this action takes us to that "page" now
         
